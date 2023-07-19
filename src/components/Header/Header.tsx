65cdfaed0135fd83386fe7aaf17e011101d8db77
Botabay <tohgl49@gmail.com>
2023-07-20 00:41:27 +0600

    fixed a previous commit with a unworkable css modules

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import s from './Header.module.css'
 export const Header = (): JSX.Element => {
     return (
         <header className={s.header}>
+            
             <Navbar />
         </header>
     )

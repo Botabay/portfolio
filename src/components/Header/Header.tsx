2fcccce445b779716e9d95517d94c438b358cbeb
Botabay <tohgl49@gmail.com>
2023-07-19 23:57:29 +0600

    css module files do not work

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { Navbar } from "./Navbar/Navbar"
 import s from './Header.module.css'
 
-export const Header=()=>{
+export const Header = (): JSX.Element => {
     return (
         <header className={s.header}>
             <Navbar />
